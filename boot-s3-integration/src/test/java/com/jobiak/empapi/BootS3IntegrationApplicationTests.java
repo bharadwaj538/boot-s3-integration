c3277e65a32b7f9102da8b80ed11fc459616b22b
bharadwajpudi <bharadwajpudi2018@gmail.com>
2022-05-17 11:52:10 +0530

    Boot S3 Integration Done

---
+++ boot-s3-integration/src/test/java/com/jobiak/empapi/BootS3IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobiak.empapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootS3IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
